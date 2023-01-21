f379541d50db7136ca2649e2707c854c3c1702c5
Bartek <evoarcadias@gmail.com>
2023-01-21 16:37:22 +0100

    Added simple block class

--- tetris.py
+++ tetris.py
@@ -1,18 +1,22 @@
 from settings import *
 import math
 from tetronimo import Tetronimo
+import pygame as pg
 
 class Tetris:
     def __init__(self, app) -> None:
         self.app = app
+        self.sprite_group = pg.sprite.Group()
         self.tetronimo = Tetronimo(self)
 
     def update(self):
         self.tetronimo.update()
+        self.sprite_group.update()
         pass
 
     def draw(self):
         self.draw_grid()
+        self.sprite_group.draw(self.app.screen)
         pass
 
     def draw_grid(self):
--- tetronimo.py
+++ tetronimo.py
@@ -1,15 +1,24 @@
 import pygame as pg
 from settings import *
 
-class Block:
-    def __init__(self) -> None:
-        pass
-    
+
+class Block(pg.sprite.Sprite):
+    def __init__(self, tetronimo, pos) -> None:
+        self.tetronimo = tetronimo
+
+        super().__init__(tetronimo.tetris.sprite_group)
+        self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
+        self.image.fill("orange")
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos[0] * TILE_SIZE, pos[1] * TILE_SIZE
+
+
 class Tetronimo:
-    def __init__(self,tetris) -> None:
+    def __init__(self, tetris) -> None:
         self.tetris = tetris
+        Block(self, (4, 5))
         pass
-    
+
     def update(self):
         pass
-    
