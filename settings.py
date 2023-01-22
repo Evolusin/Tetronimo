28845f6c059f4783b522c4c0d8650a72817ab362
Bartek <evoarcadias@gmail.com>
2023-01-22 23:30:19 +0100

    Added random generating tetris blocks

--- settings.py
+++ settings.py
@@ -1,8 +1,25 @@
 import pygame as pg
 
+
+vec = pg.math.Vector2
+
 FPS = 60
 FIELD_COLOR = (48, 39, 32)
 
 TILE_SIZE = 40
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 16
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
+INIT_VEC_POS = vec(FIELD_SIZE) // 2
+
+MOVE_DIRECTIONS = {"LEFT": vec(-1, 0), "RIGHT": vec(1, 0), "DOWN": vec(0, 1)}
+
+BLOCKS = {
+    "T": [(0, 0), (-1, 0), (1, 0), (0, -1)],
+    "O": [(0, 0), (0, -1), (1, 0), (1, -1)],
+    "J": [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    "L": [(0, 0), (1, 0), (0, -1), (0, -2)],
+    "I": [(0, 0), (0, 1), (0, -1), (0, -2)],
+    "S": [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    "Z": [(0, 0), (1, 0), (0, -1), (-1, -1)],
+}
--- tetronimo.py
+++ tetronimo.py
@@ -1,23 +1,25 @@
 import pygame as pg
 from settings import *
-
+import random
 
 class Block(pg.sprite.Sprite):
     def __init__(self, tetronimo, pos) -> None:
         self.tetronimo = tetronimo
+        self.pos = vec(pos) + INIT_VEC_POS
 
         super().__init__(tetronimo.tetris.sprite_group)
         self.image = pg.Surface([TILE_SIZE, TILE_SIZE])
         self.image.fill("orange")
 
         self.rect = self.image.get_rect()
-        self.rect.topleft = pos[0] * TILE_SIZE, pos[1] * TILE_SIZE
+        self.rect.topleft = self.pos * TILE_SIZE
 
 
 class Tetronimo:
     def __init__(self, tetris) -> None:
         self.tetris = tetris
-        Block(self, (4, 5))
+        self.shape = random.choice(list(BLOCKS.keys()))
+        self.blocks = [Block(self,pos) for pos in BLOCKS[self.shape]]
         pass
 
     def update(self):
